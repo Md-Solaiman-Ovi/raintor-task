f9b09da2fe126422414d17290b8b971e602c20e1
ovi <msovi480@gmail.com>
2025-06-20 12:21:39 +0600

    fix: leaflet css is missing in production

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,23 +8,25 @@ const Header: React.FC = () => {
   return (
     <header className="flex items-center justify-between px-8 py-6">
       <h1 className="text-xl font-bold">DEVLOP.ME</h1>
-      <nav className="hidden md:flex space-x-6 text-sm">
-        <button onClick={() => navigate("/")} className="hover:underline">
-          Home
-        </button>
-        <a href="#" className="hover:underline">
-          About
-        </a>
-        <a href="#" className="hover:underline">
-          Portfolio
-        </a>
-        <a href="#" className="hover:underline">
-          Blog
-        </a>
-      </nav>
-      <div className="flex items-center space-x-4">
-        <ThemeToggle />
-        <Button variant="outline">Start Project</Button>
+      <div className="flex items-center gap-10">
+        <nav className="hidden md:flex space-x-6 text-sm">
+          <a href="#" onClick={() => navigate("/")} className="hover:underline">
+            Home
+          </a>
+          <a href="#" className="hover:underline">
+            About
+          </a>
+          <a href="#" className="hover:underline">
+            Portfolio
+          </a>
+          <a href="#" className="hover:underline">
+            Blog
+          </a>
+        </nav>
+        <div className="flex items-center space-x-4">
+          <ThemeToggle />
+          <Button variant="outline">Start Project</Button>
+        </div>
       </div>
     </header>
   );
